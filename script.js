0c1d18d45c305e6aaf08f74f549aa9563b3e1c87
Miles <32055007+Persistr@users.noreply.github.com>
2019-09-27 11:12:52 +0800

    增加文字效果

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // coded by @lasjorg
 // eslint-disable-next-line no-unused-vars
 const projectName = 'portfolio';
+
+$(function(){
+    var cont=0;
+    var font=$("#text").html();
+    function run(){
+        $("#text").html(font.substring(0,cont++));
+    }
+    setInterval(run, 1000);
+})
