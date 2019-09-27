8a43ad04094b62516820648a370b3349e4b981f5
Miles <32055007+Persistr@users.noreply.github.com>
2019-09-27 12:29:54 +0800

    bug fixed

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 // eslint-disable-next-line no-unused-vars
 const projectName = 'portfolio';
 
-$(function(){
-    var cont=0;
-    var font=$("#text").html();
-    function run(){
-        $("#text").html(font.substring(0,cont++));
-    }
-    setInterval(run, 1000);
-})
+
+$(document).ready(function() {
+    $(function(){
+        var cont=0;
+        var font=$("#text").html();
+        function run(){
+            $("#text").html(font.substring(0,cont++));
+        }
+        setInterval(run, 1000);
+    })
+    
+});
