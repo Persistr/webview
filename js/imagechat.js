ebb3dad2c35d8f021f24347d5b26e7373f0a1cac
Miles <32055007+Persistr@users.noreply.github.com>
2019-10-08 15:56:12 +0800

    add save

--- js/imagechat.js
+++ js/imagechat.js
@@ -1,3 +1,5 @@
+Bmob.initialize("b74ac2feaf9f7003", "4182b3d23204544c67ed307bcfa7318c");
+
 $(document).ready(function() {
     $(function () {
         function chooseImage(fileid, imgid, fileValId) {
@@ -10,9 +12,9 @@ $(document).ready(function() {
             var reader = new FileReader();
             reader.readAsDataURL(file);
             reader.onload = function (e) {
-                alert(reader.result);
                 var imgResult = e.target.result;
-                alert(imgResult);
+                saveImage(imgResult);
+                
                 var imgObj = document.getElementById(imgid);
                 if (typeof (imgObj) != "undefined") {
                     imgObj.setAttribute("src", imgResult);
@@ -23,6 +25,17 @@ $(document).ready(function() {
                 }
             };
         };
+
+        function saveImage(base64img) {
+            const query = Bmob.Query('tableName');
+            query.set("base64img", base64img)
+            query.save().then(res => {
+                console.log(res)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+
         $('#file').on('change', function () {
             chooseImage('file', 'photo', 'fileVal');
         });
