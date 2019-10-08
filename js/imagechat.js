efb91e80ec48912d4f10bac93068a28d4c009833
Miles <32055007+Persistr@users.noreply.github.com>
2019-10-08 15:16:17 +0800

    imagechat add

---
+++ js/imagechat.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $(function () {
+        function chooseImage(fileid, imgid, fileValId) {
+            var fileObj = document.getElementById(fileid); 
+            if (typeof (fileObj) == "undefined" || fileObj.files.length == 0) {
+                 console.log('file ' + fileid + ' not exists');
+                 return;
+            }
+            var file = fileObj.files[0];
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function (e) {
+                var imgResult = e.target.result;
+                var imgObj = document.getElementById(imgid);
+                if (typeof (imgObj) != "undefined") {
+                    imgObj.setAttribute("src", imgResult);
+                }
+                var fileValObj = document.getElementById(fileValId);
+                if (typeof (fileValObj) != "undefined") {
+                    fileValObj.setAttribute("value", imgResult);
+                }
+            };
+        };
+        $('#file').on('change', function () {
+            chooseImage('file', 'photo', 'fileVal');
+        });
+    });
+    
+});
