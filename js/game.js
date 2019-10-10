d501755bb8255475394e45231af418f3f5c86e06
Miles <32055007+Persistr@users.noreply.github.com>
2019-10-10 16:56:18 +0800

    add

--- js/game.js
+++ js/game.js
@@ -9,7 +9,11 @@ $(document).ready(function() {
             query.equalTo("ID","==",id);
             query.find().then(res => {
                 //更新数据
-                document.getElementById('name').innerText = '姓名' + res.name;
+                document.getElementById('name').innerText = '姓名:' + res[0].name;
+                document.getElementById('rank').innerText = '等级:' + res[0].rank;
+                document.getElementById('hp').innerText = '生命值:' + res[0].HP;
+                document.getElementById('attack').innerText = '攻击力:' + res[0].attack;
+                document.getElementById('defense').innerText = '防御力:' + res[0].defense;
                 console.log(res);
             });
             
