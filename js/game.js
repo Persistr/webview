fc2b796fb23f7a346f4653b9fe249f9df7cd2815
Miles <32055007+Persistr@users.noreply.github.com>
2019-10-10 17:01:53 +0800

    fixed

--- js/game.js
+++ js/game.js
@@ -8,12 +8,20 @@ $(document).ready(function() {
             const query = Bmob.Query("UserTable");
             query.equalTo("ID","==",id);
             query.find().then(res => {
-                //更新数据
-                document.getElementById('name').innerText = '姓名:' + res[0].name;
-                document.getElementById('rank').innerText = '等级:' + res[0].rank;
-                document.getElementById('hp').innerText = '生命值:' + res[0].HP;
-                document.getElementById('attack').innerText = '攻击力:' + res[0].attack;
-                document.getElementById('defense').innerText = '防御力:' + res[0].defense;
+                if(res.length > 0) {
+                    //更新数据
+                    document.getElementById('name').innerText = '姓名:' + res[0].name;
+                    document.getElementById('rank').innerText = '等级:' + res[0].rank;
+                    document.getElementById('hp').innerText = '生命值:' + res[0].HP;
+                    document.getElementById('attack').innerText = '攻击力:' + res[0].attack;
+                    document.getElementById('defense').innerText = '防御力:' + res[0].defense;
+                }else {
+                    $.alert({
+                        title: 'Msg!',
+                        content: '没有该账户信息!',
+                    });
+                }
+                
                 console.log(res);
             });
             
