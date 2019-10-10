34c4a9a0087c1b4bc9274daf87b2acf0039a3939
Miles <32055007+Persistr@users.noreply.github.com>
2019-10-10 16:43:53 +0800

    读数据 test

--- js/game.js
+++ js/game.js
@@ -2,7 +2,24 @@ Bmob.initialize("e59aaec7293dfd82", "as582d");
 
 $(document).ready(function() {
     $(function(){
-        
+        //读取玩家信息 传入id
+        function readInfo() {
+            var id = document.getElementById('userId').value;
+            const query = Bmob.Query("UserTable");
+            query.equalTo("ID","==",id);
+            query.find().then(res => {
+                //更新数据
+                
+                console.log(res);
+            });
+            
+        }
+
+
+
+        $('#read').on('click', function() {
+            readInfo();
+        })
     })
     
 });
