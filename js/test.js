e58f437115a2cbc157fdf1253f67713370f89961
Persistr <755842816@qq.com>
2020-05-19 22:10:39 +0800

    test
    test

---
+++ js/test.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(function(){
+        window.onload = function() {
+
+        }
+        function clic(){
+            for(var i=0;i<5;i++) {
+                document.getElementById("content").innerHTML += "</br><input name=\"按钮\" type=\"button\" onClick=\"buttonClick()\" value=\"按钮\"><\/input>";
+            }
+            
+        }
+
+        function buttonClick() {
+            alert('HAHAHA')
+        }
+    })
+    
+});
+
