f8ee5c6d8af443e539dc4bf4e180f957c3d2c258
Persistr <755842816@qq.com>
2020-05-19 22:19:31 +0800

    1

--- js/test.js
+++ js/test.js
@@ -4,15 +4,20 @@ $(document).ready(function() {
             clic()
         }
         function clic(){
+
             for(var i=0;i<5;i++) {
-                document.getElementById("content").innerHTML += "</br><input name=\"按钮\" type=\"button\" onClick=\"a()\" value=\"按钮\"><\/input>";
+                var MyDiv = document.getElementById("content");
+                var bt = document.createElement("button");
+                bt.innerHTML = 'tiye';
+                bt.onclick = function() {
+                    alert('tianyehhh')
+                }
+                MyDiv.appendChild(bt);
             }
             
         }
 
-        function a() {
-            alert('HAHAHA')
-        }
+        
     })
     
 });
