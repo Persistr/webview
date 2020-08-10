b638c1e195ba3a29170c8ba78c95b3962202ef05
Persistr <755842816@qq.com>
2020-08-10 17:54:12 +0800

    Update jkm.js

--- js/jkm.js
+++ js/jkm.js
@@ -112,10 +112,10 @@ function getQueryVariable() {
     for (var i=0;i<vars.length;i++) {
             var pair = vars[i].split("=");
             if(pair[0] == 'name'){
-                return pair[1];
+                name = pair[1];
             }
             if(pair[0] == 'place'){
-                return pair[1];
+                place = pair[1];
             }
             
     }
